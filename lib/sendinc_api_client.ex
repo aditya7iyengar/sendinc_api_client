5448a6c8e7f67fad5d225f2f7ed70c69cabce464
Adi Iyengar <aditya7iyengar@gmail.com>
2018-03-18 16:16:57 -0400

    Prepare for release

--- lib/sendinc_api_client.ex
+++ lib/sendinc_api_client.ex
@@ -2,17 +2,4 @@ defmodule SendincApiClient do
   @moduledoc """
   Documentation for SendincApiClient.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SendincApiClient.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- mix.exs
+++ mix.exs
@@ -1,29 +1,63 @@
 defmodule SendincApiClient.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/aditya7iyengar/sendinc_api_client"
+
   def project do
     [
       app: :sendinc_api_client,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env == :prod,
+      deps: deps(),
+
+      # Hex
+      description: description(),
+      package: package(),
+
+      # Docs
+      name: "SendInc API Client",
+      docs: docs(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Adi Iyengar"],
+      licenses: ["MIT"],
+      links: %{"Github" => @url},
+    ]
+  end
+
   defp deps do
     [
       {:ecto, "~> 2.2"},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
       {:httpoison, "~> 1.0"},
       {:poison, "~> 3.1"},
+    ]
+  end
+
+  defp description do
+    """
+    An elixir wrapper around SendInc API Client
+    """
+  end
+
+  def docs do
+    [
+      main: "Sendinc.Curl",
+      source_url: @url,
+      source_ref: "v#{@version}"
     ]
   end
 end
